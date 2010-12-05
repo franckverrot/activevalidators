fccd8b3d1956f0b4b6ddb59027c179db1c5ba969
Oriol Gual <oriol.gual@gmail.com>
2010-12-05 12:13:57 +0100

    Refactor Respond To Validator specs

--- spec/models/respond_to_validator_model.rb
+++
@@ -1,7 +0,0 @@
-module Models
-  class RespondToValidatorModel
-    include ActiveModel::Validations
-    attr_accessor :responder, :global_condition, :local_condition
-    validates :responder, :respond_to => { :call => true, :if => :local_condition }, :if => :global_condition
-  end
-end
--- spec/specs/respond_to_spec.rb
+++ spec/specs/respond_to_spec.rb
@@ -1,33 +1,37 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
 describe "Respond To Validation" do
-  it "respond_to?" do
-    model = Models::RespondToValidatorModel.new
-    model.responder = lambda {}
-    model.global_condition = true
-    model.local_condition = true
+  before(:each) do
+    TestRecord.reset_callbacks(:validate)
+    TestRecord.validates :responder, :respond_to => { :call => true, :if => :local_condition }, :if => :global_condition
+  end
+  
+  subject { TestRecord.new }
 
-    model.valid?.should be(true)
-    model.should have(0).errors
+  it "respond_to?" do
+    subject.responder = lambda {}
+    subject.global_condition = true
+    subject.local_condition = true
+
+    subject.should be_valid
+    subject.should have(0).errors
   end
 
   describe "when does not respond_to?" do
-    let(:model) do
-      Models::RespondToValidatorModel.new.tap do |m|
-        m.responder        = 42
-        m.global_condition = true
-        m.local_condition  = true
-      end
+    before :each do
+      subject.responder        = 42
+      subject.global_condition = true
+      subject.local_condition  = true
     end
 
     it "rejects the responder" do
-      model.valid?.should be(false)
-      model.should have(1).errors
+      subject.should_not be_valid
+      subject.should have(1).error
     end
 
     it "generates an error message of type invalid" do
-      model.valid?.should be(false)
-      model.errors[:responder].should == [model.errors.generate_message(:responder, :invalid)]
+      subject.should_not be_valid
+      subject.errors[:responder].should include subject.errors.generate_message(:responder, :invalid)
     end
   end
 end
