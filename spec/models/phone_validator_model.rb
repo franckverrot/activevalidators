552916de1a3115dfc2bf660785614a67b480cd86
Oriol Gual <oriol.gual@gmail.com>
2010-12-05 12:09:47 +0100

    Refactor Phone Validator specs

--- spec/models/phone_validator_model.rb
+++
@@ -1,7 +0,0 @@
-module Models
-  class PhoneValidatorModel
-    include ActiveModel::Validations
-    attr_accessor :phone
-    validates :phone, :phone => true
-  end
-end
--- spec/specs/phone_spec.rb
+++ spec/specs/phone_spec.rb
@@ -2,56 +2,56 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper.rb')
 
 describe "Phone Validation" do
 
+  before(:each) do
+    TestRecord.reset_callbacks(:validate)
+    TestRecord.validates :phone, :phone => true
+  end
+  
+  subject { TestRecord.new }
+
   it 'should validate format of phone with ###-###-####' do
-    model = Models::PhoneValidatorModel.new
-    model.phone = '999-999-9999'
-    model.valid?.should be(true)
-    model.should have(0).errors
+    subject.phone = '999-999-9999'
+    subject.should be_valid
+    subject.should have(0).errors
   end
 
   it 'should validate format of phone with ##########' do
-    model = Models::PhoneValidatorModel.new
-    model.phone = '9999999999'
-    model.valid?.should be(true)
-    model.should have(0).errors
+    subject.phone = '9999999999'
+    subject.should be_valid
+    subject.should have(0).errors
   end
 
   it 'should validate format of phone with ###.###.####' do
-    model = Models::PhoneValidatorModel.new
-    model.phone = '999.999.9999'
-    model.valid?.should be(true)
-    model.should have(0).errors
+    subject.phone = '999.999.9999'
+    subject.should be_valid
+    subject.should have(0).errors
   end
 
   it 'should validate format of phone with ### ### ####' do
-    model = Models::PhoneValidatorModel.new
-    model.phone = '999 999 9999'
-    model.valid?.should be(true)
-    model.should have(0).errors
+    subject.phone = '999 999 9999'
+    subject.should be_valid
+    subject.should have(0).errors
   end
 
   it 'should validate format of phone with (###) ###-####' do
-    model = Models::PhoneValidatorModel.new
-    model.phone = '(999) 999-9999'
-    model.valid?.should be(true)
-    model.should have(0).errors
+    subject.phone = '(999) 999-9999'
+    subject.should be_valid
+    subject.should have(0).errors
   end
 
   describe "for invalid formats" do
-    let(:model) do
-      Models::PhoneValidatorModel.new.tap do |m|
-        m.phone = '999'
-      end
+    before :each do
+      subject.phone = '999'
     end
 
     it "rejects invalid formats" do
-      model.valid?.should be(false)
-      model.should have(1).errors
+      subject.should_not be_valid
+      subject.should have(1).error
     end
 
     it "generates an error message of type invalid" do
-      model.valid?.should be(false)
-      model.errors[:phone].should == [model.errors.generate_message(:phone, :invalid)]
+      subject.should_not be_valid
+      subject.errors[:phone].should include subject.errors.generate_message(:phone, :invalid)
     end
   end
 end
