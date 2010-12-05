5874d899f4287122b12d3eb261d12c8d37eae9a8
Oriol Gual <oriol.gual@gmail.com>
2010-12-05 11:33:11 +0100

    Add validity check standard to IPValidator and refactor

--- lib/active_model/validations/ip_validator.rb
+++ lib/active_model/validations/ip_validator.rb
@@ -2,18 +2,23 @@ module ActiveModel
   module Validations
     class IpValidator < EachValidator
       def validate_each(record, attribute, value)
-        regex = case options[:format]
-        when :v4
-          ipv4_regex
-        when :v6
-          ipv6_regex
-        end
-
-        raise "Unknown IP validator format #{options[:format].inspect}" unless regex
         record.errors.add(attribute) unless regex.match(value)
       end
 
+      def check_validity!
+        raise ArgumentError, "Unknown IP validator format #{options[:format].inspect}" unless [:v4, :v6].include? options[:format]
+      end
+
       private
+        def regex
+          case options[:format]
+          when :v4
+            ipv4_regex
+          when :v6
+            ipv6_regex
+          end
+        end
+
         def ipv4_regex
           # Extracted from ruby 1.9.2
           regex256 = 
