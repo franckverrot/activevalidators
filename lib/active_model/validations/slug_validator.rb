b0ff7da89fe7179d11a6c7c5d346118afc554bb9
Franck Verrot <franck@verrot.fr>
2010-12-08 08:40:30 +0100

    Added :blank error message

--- lib/active_model/validations/slug_validator.rb
+++ lib/active_model/validations/slug_validator.rb
@@ -2,7 +2,9 @@ module ActiveModel
   module Validations
     class SlugValidator < EachValidator
       def validate_each(record, attribute, value)
-        if value && value != value.parameterize
+        if value.nil?
+          record.errors.add_on_blank(attribute)
+        elsif value != value.parameterize
           record.errors.add(attribute)
         end
       end
