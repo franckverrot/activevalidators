74105068baff4403871513809cd17546e9de690e
Renato Riccieri <renato.riccieri@gmail.com>
2011-03-16 02:10:22 -0300

    Refactored phone validator

--- lib/active_model/validations/phone_validator.rb
+++ lib/active_model/validations/phone_validator.rb
@@ -2,10 +2,32 @@ module ActiveModel
   module Validations
     class PhoneValidator < EachValidator
       def validate_each(record, attribute, value)
-        unless value =~ /^\d{3}-\d{3}-\d{4}|\d{3}\.\d{3}\.\d{4}|\d{10}|\d{3}\s\d{3}\s\d{4}|\(\d{3}\)\s\d{3}-\d{4}$/i
+        @value = value
+        @formats = PhoneValidator.known_formats[options[:country]] || PhoneValidator.known_formats[:us]
+        unless matches_any?
           record.errors.add(attribute)
         end
       end
+
+      def self.known_formats
+        @@known_formats ||=
+        {:us     => ["###-###-####", "##########", "###.###.####", "### ### ####", "(###) ###-####"],
+         :brazil => ["## ####-####", "(##) ####-####", "##########"],
+         :france => ["## ## ## ## ##"],
+         :uk     => ["#### ### ####"]}
+      end
+
+      def matches_any?
+        false if @formats.nil? or not @formats.respond_to?(:detect)
+        @formats.detect { |format| @value.match(PhoneValidator.regexp_from format) }
+      end
+
+      private
+
+      def self.regexp_from(format)
+        Regexp.new "^"+(Regexp.escape format).gsub('\#','\d')+"$"
+      end
+
     end
   end
 end
