aad00d76ffb316c665fa1bd5ec364a9e1b48e1dd
Brian Moseley <bcm@maz.org>
2011-05-12 14:44:32 -0700

    add ups tracking number validation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,5 @@ class TestRecord
   include ActiveModel::Validations
   attr_accessor :ip, :url, :slug, :responder, :global_condition,
     :local_condition, :phone, :email, :card, :password, :twitter_username,
-    :postal_code
+    :postal_code, :tracking_number
 end
