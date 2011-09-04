a81a14e3bc4d0a6feb7744b755a8d3120a24f390
Franck Verrot <franck@verrot.fr>
2011-09-04 08:34:33 +0200

    Fooling around again for the CI

--- test/validations/email_test.rb
+++ test/validations/email_test.rb
@@ -4,41 +4,42 @@ describe "Email Validation" do
   before do
     TestRecord.reset_callbacks(:validate)
     TestRecord.validates :email, :email => true
-    @subject = TestRecord.new
   end
-  
 
   it "accepts valid emails" do
-    @subject.email = 'franck@verrot.fr'
-    @subject.valid?.must_equal true
-    @subject.errors.size.must_equal 0
+    subject = build_email_record :email => 'franck@verrot.fr'
+    subject.valid?.must_equal true
+    subject.errors.size.must_equal 0
   end
 
   it "accepts complete emails" do
-    @subject.email = 'Mikel Lindsaar (My email address) <mikel@test.lindsaar.net>'
-    @subject.valid?.must_equal true
-    @subject.errors.size.must_equal 0
+    subject = build_email_record :email => 'Mikel Lindsaar (My email address) <mikel@test.lindsaar.net>'
+    subject.valid?.must_equal true
+    subject.errors.size.must_equal 0
   end
 
   describe "for invalid emails" do
-    before :each do
-      @subject.email = 'franck.fr'
-    end
-
     it "rejects invalid emails" do
-      @subject.valid?.must_equal false
-      @subject.errors.size.must_equal 1
+      subject = build_email_record :email => 'franck.fr'
+      subject.valid?.must_equal false
+      subject.errors.size.must_equal 1
     end
 
     it 'rejects local emails' do
-      @subject.email = 'franck'
-      @subject.valid?.must_equal false
-      @subject.errors.size.must_equal 1
+      subject = build_email_record :email => 'franck.fr'
+      subject.email = 'franck'
+      subject.valid?.must_equal false
+      subject.errors.size.must_equal 1
     end
 
     it "generates an error message of type invalid" do
-      @subject.valid?.must_equal false
-      @subject.errors[:email].include?(@subject.errors.generate_message(:email, :invalid)).must_equal true
+      subject = build_email_record :email => 'franck.fr'
+      subject.valid?.must_equal false
+      subject.errors[:email].include?(subject.errors.generate_message(:email, :invalid)).must_equal true
     end
+  end
+
+  def build_email_record(attrs = {})
+    TestRecord.new attrs
   end
 end
