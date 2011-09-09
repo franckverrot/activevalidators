02e5cf6882daf7fbf0b8798a05babfb74899dfb6
Franck Verrot <franck@verrot.fr>
2011-09-09 23:06:38 +0200

    Merged @utahstreetlabs' work on the tracking number validator.
    I had to rebase the work and port the test cases to MiniTest but I think it's OK now. Thanks! <3

--- lib/active_model/validations/tracking_number_validator.rb
+++ lib/active_model/validations/tracking_number_validator.rb
@@ -2,30 +2,67 @@ module ActiveModel
   module Validations
     class TrackingNumberValidator < EachValidator
       def validate_each(record, attribute, value)
-        @value = value
-        carrier = options[:carrier]
+        carrier = options[:carrier] || (options[:carrier_field] && record.send(options[:carrier_field]))
         raise "Carrier option required" unless carrier
-        @formats = TrackingNumberValidator.known_formats[carrier]
-        raise "No known tracking number formats for carrier #{carrier}" unless @formats
-        record.errors.add(attribute) unless matches_any?
+        method = "valid_#{carrier.to_s}?"
+        raise "Tracking number validation not supported for carrier #{carrier}" unless self.respond_to?(method)
+        record.errors.add(attribute) unless self.send(method, value)
       end
 
-      def self.known_formats
-        @@known_formats ||= {
-          # see https://www.ups.com/content/us/en/tracking/help/tracking/tnh.html
-          :ups => ['1Z................', '............', 'T..........', '.........'],
-        }
+      # UPS:
+      # ups tracking codes are validated solely on their format
+      # see https://www.ups.com/content/us/en/tracking/help/tracking/tnh.html
+      UPS_REGEXES = [ /^1Z[a-zA-Z0-9]{16}$/, /^[a-zA-Z0-9]{12}$/, /^[a-zA-Z0-9]{9}$/, /^T[a-zA-Z0-9]{10}$/ ]
+      def valid_ups?(value)
+        !!UPS_REGEXES.detect { |fmt| value.match(fmt) }
       end
 
-      def matches_any?
-        false if @formats.nil? or not @formats.respond_to?(:detect)
-        @formats.detect { |format| @value.match(TrackingNumberValidator.regexp_from format) }
+      # USPS:
+      # usps tracking codes are validated based on format (one of USS228 or USS39)
+      # and a check digit (using either of the USPS's MOD10 or MOD11 algorithms)
+      # see USPS Publications:
+      #  -  #91 (05/2008) pp. 38
+      #  -  #97 (05/2002) pp. 62-63
+      #  - #109 (09/2007) pp. 19-21
+      def valid_usps?(value)
+        uss228?(value) || uss39?(value)
       end
 
-      private
+      USS128_REGEX = /^(\d{19,21})(\d)$/
+      def uss228?(value)
+        m = value.match(USS128_REGEX)
+        m.present? && (m[2].to_i == usps_mod10(m[1]))
+      end
 
-      def self.regexp_from(format)
-        Regexp.new "^"+(Regexp.escape format).gsub('\#','\d').gsub('\.','[a-zA-Z0-9]')+"$"
+      USS39_REGEX = /^[a-zA-Z0-9]{2}(\d{8})(\d)US$/
+      def uss39?(value)
+        m = value.match(USS39_REGEX)
+        # it appears to be valid for a USS39 barcode's checkdigit to be calculated with either the usps mod 10
+        # algorithm or the usps mod 11.
+        m.present? && (m[2].to_i == usps_mod10(m[1]) || m[2].to_i == usps_mod11(m[1]))
+      end
+
+      MOD10_WEIGHTS = [3,1]
+      def usps_mod10(chars)
+        10 - weighted_sum(chars.reverse, MOD10_WEIGHTS) % 10
+      end
+
+      MOD11_WEIGHTS = [8,6,4,2,3,5,9,7]
+      def usps_mod11(chars)
+        mod = weighted_sum(chars, MOD11_WEIGHTS) % 11
+        case mod
+        when 0 then 5
+        when 1 then 0
+        else 11 - mod
+        end
+      end
+
+      # takes a string containing digits and calculates a checksum using the provided weight array
+      # cycles the weight array if it's not long enough
+      def weighted_sum(value, weights)
+        digits = value.split('').map { |d| d.to_i }
+        weights = weights.cycle.take(digits.count) if weights.count < digits.count
+        digits.zip(weights).inject(0) { |s,p| s + p[0] * p[1] }
       end
     end
   end
--- test/validations/tracking_number_test.rb
+++ test/validations/tracking_number_test.rb
@@ -1,59 +1,151 @@
 require 'test_helper.rb'
 
 describe "Tracking Number Validation" do
-  def build_tracking_number_record tracking_number, attrs = {}
+  def build_tracking_number_record carrier_opts, attrs = {}
     TestRecord.reset_callbacks(:validate)
-    TestRecord.validates :tracking_number, :tracking_number => tracking_number
-    TestRecord.new attrs
+    TestRecord.validates :tracking_number, :tracking_number => carrier_opts
+    TestRecord.new(attrs).tap do |record|
+      yield record if block_given?
+    end
+  end
+
+  def assert_valid_tracking_number(carrier_opts, tracking_number, &block)
+    subject = build_tracking_number_record(carrier_opts, &block)
+    subject.tracking_number = tracking_number
+    subject.valid?.must_equal true
+    subject.errors.size.must_equal 0
+  end
+
+  def assert_invalid_tracking_number(carrier_opts, tracking_number, &block)
+    subject = build_tracking_number_record(carrier_opts, &block)
+    subject.tracking_number = tracking_number
+    subject.valid?.must_equal false
+    subject.errors.size.must_equal 1
+    subject.errors[:tracking_number].must_include subject.errors.generate_message(:tracking_number, :invalid)
   end
 
   describe "when no carrier parameter is given" do
     it "raises an exception" do
-      assert_raises(RuntimeError, "Carrier option required") { build_tracking_number_record(true).valid? }
+      assert_raises(RuntimeError, "Carrier option required") { build_tracking_number_record({:carrier=>true}).valid? }
     end
   end
 
   describe "when given a ups carrier parameter" do
     it 'should validate format of tracking number with 1Z................' do
-      subject = build_tracking_number_record({:carrier => :ups})
-      subject.tracking_number = '1Z12345E0205271688'
-      subject.valid?.must_equal true
-      subject.errors.size.must_equal 0
+      assert_valid_tracking_number({:carrier => :ups}, '1Z12345E0205271688')
     end
 
     it 'should validate format of tracking number with ............' do
-      subject = build_tracking_number_record({:carrier => :ups})
-      subject.tracking_number = '9999V999J999'
-      subject.valid?.must_equal true
-      subject.errors.size.must_equal 0
+      assert_valid_tracking_number({:carrier => :ups}, '9999V999J999')
     end
 
     it 'should validate format of tracking number with T..........' do
-      subject = build_tracking_number_record({:carrier => :ups})
-      subject.tracking_number = 'T99F99E9999'
-      subject.valid?.must_equal true
-      subject.errors.size.must_equal 0
+      assert_valid_tracking_number({:carrier => :ups}, 'T99F99E9999')
     end
 
     it 'should validate format of tracking number with .........' do
-      subject = build_tracking_number_record({:carrier => :ups})
-      subject.tracking_number = '990728071'
-      subject.valid?.must_equal true
-      subject.errors.size.must_equal 0
+      assert_valid_tracking_number({:carrier => :ups}, '990728071')
+    end
+
+    describe "for invalid formats" do
+      it "rejects invalid formats and generates an error message of type invalid" do
+        assert_invalid_tracking_number({:carrier => :ups}, '1Z12345E020_271688')
+      end
     end
   end
 
-  describe "for invalid formats" do
-    it "rejects invalid formats" do
-      subject = build_tracking_number_record({:carrier => :ups}, :tracking_number => '1Z12345E020_271688')
-                                              subject.valid?.must_equal false
-                                              subject.errors.size.must_equal 1
+  describe "when given a usps carrier parameter" do
+    describe "with valid formats" do
+      it 'USS39 tracking number with valid MOD10 check digit' do
+        assert_valid_tracking_number({:carrier => :usps}, 'EA123456784US')
+      end
+
+      it 'USS39 tracking number with valid MOD11 check digit' do
+        assert_valid_tracking_number({:carrier => :usps}, 'RB123456785US')
+      end
+
+      it '20 character USS128 tracking number with valid MOD10 check digit' do
+        assert_valid_tracking_number({:carrier => :usps}, '71123456789123456787')
+      end
+
+      it '22 character USS128 tracking number with valid MOD10 check digit' do
+        assert_valid_tracking_number({:carrier => :usps}, '9171969010756003077385')
+      end
     end
 
-    it "generates an error message of type invalid" do
-      subject = build_tracking_number_record({:carrier => :ups}, :tracking_number => '1Z12345E020_271688')
-                                              subject.valid?.must_equal false
-                                              subject.errors[:tracking_number].include?(subject.errors.generate_message(:tracking_number, :invalid)).must_equal true
+    describe "with invalid formats" do
+      it 'USS39 tracking number with invalid check digit' do
+        assert_invalid_tracking_number({:carrier => :usps}, 'EA123456782US')
+      end
+
+      it 'USS39 tracking number that is too short' do
+        assert_invalid_tracking_number({:carrier => :usps}, '123456784US')
+      end
+
+      it 'USS39 tracking number that is too long' do
+        assert_invalid_tracking_number({:carrier => :usps}, 'EAB123456784US')
+      end
+
+      it 'USS39 tracking number with non-"US" product id' do
+        assert_invalid_tracking_number({:carrier => :usps}, 'EA123456784UT')
+      end
+
+      it 'USS128 tracking number with invalid check-digit' do
+        assert_invalid_tracking_number({:carrier => :usps}, '71123456789123456788')
+      end
+
+      it 'USS128 tracking number that is too short' do
+        assert_invalid_tracking_number({:carrier => :usps}, '7112345678912345678')
+      end
+
+      it 'USS128 tracking number that is too long' do
+        assert_invalid_tracking_number({:carrier => :usps}, '711234567891234567879287')
+      end
+
+      it 'USS128 tracking number with invalid chars' do
+        assert_invalid_tracking_number({:carrier => :usps}, 'U11234567891234567879')
+      end
+    end
+  end
+
+  describe "when given a record-based carrier parameter" do
+    describe "when record gives 'ups' as carrier" do
+      it 'with valid ups tracking number' do
+        assert_valid_tracking_number({:carrier_field => :carrier}, '1Z12345E0205271688') do |subject|
+          subject.carrier = 'ups'
+        end
+      end
+
+      it 'with invalid ups tracking number' do
+        assert_invalid_tracking_number({:carrier_field => :carrier}, '1Z12__5E0205271688') do |subject|
+          subject.carrier = 'ups'
+        end
+      end
+    end
+
+    describe "when record gives 'usps' as carrier" do
+      it 'with valid usps tracking number' do
+        assert_valid_tracking_number({:carrier_field => :carrier}, 'EA123456784US') do |subject|
+          subject.carrier = 'usps'
+        end
+      end
+
+      it 'with invalid usps tracking number' do
+        assert_invalid_tracking_number({:carrier_field => :carrier}, 'EA123456784UZ') do |subject|
+          subject.carrier = 'usps'
+        end
+      end
+    end
+
+    describe "when record gives an unsupported carrier" do
+      it "raises an error when validating" do
+        assert_raises(RuntimeError, "Carrier option required") do
+          build_tracking_number_record({:carrier=>:carrier}, :tracking_number => 'EA123456784US') do |subject|
+            subject.carrier = 'fedex'
+            subject.tracking_number = 'EA123456784US'
+          end.valid?
+        end
+      end
     end
   end
 end
