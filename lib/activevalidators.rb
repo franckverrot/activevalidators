6b2ee99b35e7e92c0d7a18c564391481a7ad814c
Franck Verrot <franck@verrot.fr>
2011-09-09 23:35:47 +0200

    Let's require all the validations

--- lib/activevalidators.rb
+++ lib/activevalidators.rb
@@ -1,26 +1,18 @@
-require 'active_support'
 require 'active_model'
-require 'active_record'
-require 'active_support/all'
-require 'active_model/validations'
 
 module ActiveModel
   module Validations
-    extend ActiveSupport::Autoload
-
     def self.activevalidators
-      ['Email','Url','RespondTo','Phone','Slug','Ip','CreditCard','Date','Password','Twitter','PostalCode',
-        'TrackingNumber']
+      %w(email url respond_to phone slug ip credit_card date password twitter postal_code tracking_number)
     end
 
-    #Eager autoload the library's validators into AR::Validations
     activevalidators.each do |validator_name|
-      autoload validator_name+'Validator'
+      require "active_model/validations/#{validator_name}_validator"
     end
 
     #Defines methods like validates_credit_card
     module HelperMethods
-      ActiveModel::Validations.activevalidators.map(&:underscore).each do |validator|
+      ActiveModel::Validations.activevalidators.each do |validator|
         define_method('validates_'+validator) do |*fields|
           options ||= (fields.delete fields.find { |f| f.kind_of? Hash}) || true
           args = fields.push({ validator => options })
