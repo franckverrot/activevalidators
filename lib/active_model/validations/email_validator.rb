d77d9105e5831bfac16cadbeaf9dc055d5e941ff
Cédric FABIANSKI <cedric.fabianski@gmail.com>
2012-02-27 11:06:28 +0100

    (Feature) Added strict email notion for email_validator

--- lib/active_model/validations/email_validator.rb
+++ lib/active_model/validations/email_validator.rb
@@ -2,10 +2,21 @@ require 'mail'
 module ActiveModel
   module Validations
     class EmailValidator < EachValidator
+      EMAIL_REGEXP = %r{\A[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\z}
+      
       def validate_each(record,attribute,value)
+        opts = options.dup
+        opts[:strict] ||= false
+          
         begin
           address = Mail::Address.new(value)
-          valid = address.domain && value.include?(address.address)
+          valid   = address.domain && 
+                    value.include?(address.address) &&
+                    ( 
+                      !opts[:strict] || 
+                      address.domain.match(EMAIL_REGEXP) 
+                    )
+                    
         rescue Mail::Field::ParseError
           valid = false
         end
