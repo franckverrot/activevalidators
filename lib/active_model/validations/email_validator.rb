e24922b53475f955b0a6a261b4484a1573ec6e57
Radovan Šmitala <radovan.smitala@gmail.com>
2012-06-28 15:53:46 +0200

    email validator accept only full address
    :strict changed to :only_address due to :strict is registered word

--- lib/active_model/validations/email_validator.rb
+++ lib/active_model/validations/email_validator.rb
@@ -2,24 +2,29 @@ require 'mail'
 module ActiveModel
   module Validations
     class EmailValidator < EachValidator
-      EMAIL_REGEXP = %r{\A[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\z}
-
-      def validate_each(record,attribute,value)
-        opts = options.dup
-        opts[:strict] ||= false
-
+      def validate_each(record, attribute, value)
+        # takes from: https://github.com/hallelujah/valid_email
         begin
-          address = Mail::Address.new(value)
-          valid   = address.domain && 
-                    value.include?(address.address) &&
-                    ( 
-                      !opts[:strict] || 
-                      address.domain.match(EMAIL_REGEXP) 
-                    )
-        rescue Mail::Field::ParseError
+          mail = Mail::Address.new(value)
+          # We must check that value contains a domain and that value is an email address
+          valid = mail.domain && value.include?(mail.address)
+
+          if options[:only_address]
+            # We need to dig into treetop
+            # A valid domain must have dot_atom_text elements size > 1
+            # user@localhost is excluded
+            # treetop must respond to domain
+            # We exclude valid email values like <user@localhost.com>
+            # Hence we use m.__send__(tree).domain
+            tree = mail.__send__(:tree)
+            valid &&= (tree.domain.dot_atom_text.elements.size > 1)
+          else
+            valid &&= (mail.domain.split('.').length > 1)
+          end
+        rescue Exception => e
           valid = false
         end
-        record.errors.add(attribute) unless valid
+        record.errors.add attribute, (options[:message]) unless valid
       end
     end
   end
