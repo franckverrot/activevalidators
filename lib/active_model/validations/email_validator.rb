fdd032e6b816470f7519b860d1ed392853662900
Franck Verrot <franck@verrot.fr>
2014-02-01 22:17:13 +0100

    * email_validator.rb : Extract method in the email validator

--- lib/active_model/validations/email_validator.rb
+++ lib/active_model/validations/email_validator.rb
@@ -7,24 +7,10 @@ module ActiveModel
       end
 
       def validate_each(record, attribute, value)
-        # takes from: https://github.com/hallelujah/valid_email
         begin
           mail = Mail::Address.new(value)
-          # We must check that value contains a domain and that value is an email address
-          valid = mail.domain && value.include?(mail.address)
 
-          if options[:only_address]
-            # We need to dig into treetop
-            # A valid domain must have dot_atom_text elements size > 1
-            # user@localhost is excluded
-            # treetop must respond to domain
-            # We exclude valid email values like <user@localhost.com>
-            # Hence we use m.__send__(tree).domain
-            tree = mail.__send__(:tree)
-            valid &&= (tree.domain.dot_atom_text.elements.size > 1)
-          else
-            valid &&= (mail.domain.split('.').length > 1)
-          end
+          valid = basic_check(mail) && value.include?(mail.address)
         rescue Exception => e
           valid = false
         end
@@ -36,6 +22,25 @@ module ActiveModel
         end
 
         record.errors.add attribute, (options[:message]) unless valid
+      end
+
+      def basic_check(mail)
+        # We must check that value contains a domain and that value is an email address
+        valid = !!mail.domain
+
+        if options[:only_address]
+          # We need to dig into treetop
+          # A valid domain must have dot_atom_text elements size > 1
+          # user@localhost is excluded
+          # treetop must respond to domain
+          # We exclude valid email values like <user@localhost.com>
+          # Hence we use m.__send__(tree).domain
+          tree = mail.__send__(:tree)
+          valid &&= (tree.domain.dot_atom_text.elements.size > 1)
+        else
+          valid &&= (mail.domain.split('.').length > 1)
+        end
+        valid
       end
     end
   end
