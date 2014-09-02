cd4d004c6af2cefd7d0098eff8851c108279d784
Serj L <gelvior@gmail.com>
2014-09-02 17:21:54 +0400

    NINO validation (UK).

---
+++ lib/active_model/validations/nino_validator.rb
@@ -0,0 +1,70 @@
+module ActiveModel
+  module Validations
+    class NinoValidator < EachValidator
+      def validate_each(record, attribute, value)
+        record.errors.add(attribute) if value.blank? || !NinoValidatorUK.new(value).valid?
+      end
+    end
+
+    # The National Insurance number is a number used in the United Kingdom.
+    # The format of the number is two prefix letters, six digits, and one suffix letter. The example used is typically AB123456C.
+    # Where AB - prefix, 123456 - number, C - suffix.
+    # More details: http://en.wikipedia.org/wiki/National_Insurance_number
+    class NinoValidatorUK
+      def initialize(value)
+        @nino        = value.gsub(/\s/, '').upcase                                                  # Remove spaces (they may be) and make text to be same.
+        @first_char  = @nino[0]
+        @second_char = @nino[1]
+        @prefix      = @nino[0..1]
+        @number      = @nino[2..7]
+        @suffix      = @nino[-1]
+      end
+
+      def valid?
+        size_is?(9) && first_char_valid? && second_char_valid? && prefix_valid? &&
+        prefix_not_allocated? && prefix_not_administrative_number? && number_valid? && suffix_valid?
+      end
+
+      private
+
+      def size_is?(count)
+        @nino.size == count
+      end
+
+      def first_char_valid?
+        forbidden_chars = %w[D F I Q U V]
+        !forbidden_chars.include?(@first_char)
+      end
+
+      def second_char_valid?
+        forbidden_chars = %w[D F I Q U V O]
+        !forbidden_chars.include?(@second_char)
+      end
+
+      def prefix_valid?
+        prefix_rule = /[a-zA-Z]{2}/                                                                 # Exactly 2 alphabet chars.
+        !!(prefix_rule =~ @prefix)
+      end
+
+      def number_valid?
+        number_rule = /^[0-9]{6}$/                                                                  # Exactly 6 digits.
+        !!(number_rule =~ @number)
+      end
+
+      def suffix_valid?
+        allowed_chars = %w[A B C D]
+        allowed_chars.include?(@suffix)
+      end
+
+      def prefix_not_allocated?
+        forbidden_prefixes = %w[BG, GB, NK, KN, TN, NT, ZZ]
+        !forbidden_prefixes.include?(@prefix)
+      end
+
+      def prefix_not_administrative_number?
+        administrative_prefixes = %w[OO, CR, FY, MW, NC, PP, PY, PZ]
+        !administrative_prefixes.include?(@prefix)
+      end
+    end
+  end
+end
---
+++ test/validations/nino_test.rb
@@ -0,0 +1,109 @@
+require 'test_helper.rb'
+
+describe "NINO validations" do
+  describe "for UK" do
+    describe "for invalid" do
+      it "rejects empty nino" do
+        subject = build_nino_record({:nino => ''})
+        subject.valid?.must_equal false
+      end
+
+      it "rejects nino, which has invalid first letter" do
+        invalid_first_letter = %w[d f i q u v]
+        invalid_first_letter.each do |letter|
+          invalid_nino = letter + 'B123456C'
+          subject = build_nino_record({:nino => invalid_nino})
+          subject.valid?.must_equal false
+        end
+      end
+
+      it "rejects nino, which has invalid second letter" do
+        invalid_second_letter = %w[d f i q u v o]
+        invalid_second_letter.each do |letter|
+          invalid_nino = 'a' + letter + '123456C'
+          subject = build_nino_record({:nino => invalid_nino})
+          subject.valid?.must_equal false
+        end
+      end
+
+      it "rejects too long nino" do
+        subject = build_nino_record({:nino => 'AB123456C100200'})
+        subject.valid?.must_equal false
+      end
+
+      it "rejects nino with invalid prefix" do
+        subject = build_nino_record({:nino => '#$123456C'})
+        subject.valid?.must_equal false
+      end
+
+      it "rejects nino with invalid chars in number section" do
+        subject = build_nino_record({:nino => 'AB@2%4#6C'})
+        subject.valid?.must_equal false
+      end
+
+      it "rejects wrong suffixes" do
+        invalid_suffix = %w[E F G H] # and etc
+        invalid_suffix.each do |suffix|
+          invalid_nino = 'AB123456' + suffix
+          subject = build_nino_record({:nino => invalid_nino})
+          subject.valid?.must_equal false
+        end
+      end
+
+      it "rejects non allocated prefixes" do
+        non_allocated_prefixes = %w[bg, gb, nk, kn, tn, nt, zz]
+        non_allocated_prefixes.each do |suffix|
+          invalid_nino = suffix +'123456C'
+          subject = build_nino_record({:nino => invalid_nino})
+          subject.valid?.must_equal false
+        end
+      end
+
+      it "rejects administrative numbers" do
+        administrative_numbers = %w[oo, cr, fy, mw, nc, pp, py, pz]
+        administrative_numbers.each do |suffix|
+          invalid_nino = suffix +'123456C'
+          subject = build_nino_record({:nino => invalid_nino})
+          subject.valid?.must_equal false
+        end
+      end
+
+      it "rejects temporary numbers" do
+        subject = build_nino_record({:nino => '63T12345'})
+        subject.valid?.must_equal false
+      end
+    end
+
+    describe "for valid" do
+      it "acepts correct nino" do
+        subject = build_nino_record({:nino => 'AB123456C'})
+        subject.valid?.must_equal true
+      end
+
+      it "acepts nino, divided by spaces" do
+        subject = build_nino_record({:nino => 'AB 12 34 56 C'})
+        subject.valid?.must_equal true
+      end
+
+      it "acepts downcase nino" do
+        subject = build_nino_record({:nino => 'ab 12 34 56 c'})
+        subject.valid?.must_equal true
+      end
+
+      it "acepts correct suffixes" do
+        valid_suffix = %w[A B C D]
+        valid_suffix.each do |suffix|
+          valid_nino = 'AB123456' + suffix
+          subject = build_nino_record({:nino => valid_nino})
+          subject.valid?.must_equal true
+        end
+      end
+    end
+  end
+
+  def build_nino_record(attrs = {})
+    TestRecord.reset_callbacks(:validate)
+    TestRecord.validates :nino, :nino => true
+    TestRecord.new attrs
+  end
+end
