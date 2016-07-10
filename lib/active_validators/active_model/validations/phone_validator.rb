5935b7532e3ab64530bebe0f391e181e2f0e09a2
Ross Penman <ross@pen.mn>
2016-07-10 01:06:03 +0100

    Don't depend on countries/global being required

--- lib/active_validators/active_model/validations/phone_validator.rb
+++ lib/active_validators/active_model/validations/phone_validator.rb
@@ -4,10 +4,9 @@ module ActiveModel
   module Validations
     class PhoneValidator < EachValidator
       def validate_each(record, attribute, value)
-        country_code = Country.new(options[:country].to_s.upcase).country_code unless options[:country].blank?
+        country_code = ISO3166::Country.new(options[:country].to_s.upcase).country_code unless options[:country].blank?
         record.errors.add(attribute, options[:message]) if value.blank? || ! (options[:country].blank? ? Phony.plausible?(value) : Phony.plausible?(value, :cc => country_code) )
       end
-
     end
   end
 end
