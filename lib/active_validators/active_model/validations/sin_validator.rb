3460661e06a92502cbdaf68fada996e15b5af378
Abe Fehr <abe.fehr@gmail.com>
2019-03-25 13:10:51 -0400

    Disallow business numbers from SIN validation

--- lib/active_validators/active_model/validations/sin_validator.rb
+++ lib/active_validators/active_model/validations/sin_validator.rb
@@ -4,44 +4,45 @@ module ActiveModel
   module Validations
     class SinValidator < EachValidator
       def validate_each(record, attribute, value)
-        country = options.fetch(:country, :canada)                                                  # :canada is default.
+        country = options.fetch(:country, :canada) # :canada is default.
         record.errors.add(attribute) if value.blank? || !SinValidatorGeneral.valid?(country, value, options)
       end
     end
 
     class SinValidatorGeneral
       def self.valid?(country, value, options)
-        if country == :canada
-          SinValidatorCanada.new(value, options).valid?
-        end
+        SinValidatorCanada.new(value, options).valid? if country == :canada
       end
     end
 
     # The Social Insurance Number is a nine-digit number in the format "AAA BBB CCC".
     # The number is divided into three parts.
     # AAA - is the first, BBB - is the second and the CCC is the third.
-    # Numbers that begin with the number "9" are issued to temporary residents who are not Canadian citizens
+    # Numbers that begin with the number "9" are issued to temporary residents who are not Canadian citizens.
+    # Numbers that begin with the number "8" are issued to businesses as "Business Numbers".
     # More details: http://en.wikipedia.org/wiki/Social_Insurance_Number
     #
     # Possible flags:
     #   allow_permanent_residents - citizens with cars, which begins with "9" are valid. By default it is false.
+    #   allow_business_numbers - business numbers, which begins with "8" are valid. By default it is false.
     #
-    #   validates :sin, :sin => {:country => :canada, :country_options => {:allow_permanent_residents => true}}
+    #   validates :sin, :sin => {:country => :canada, :country_options => {:allow_permanent_residents => true, :allow_business_numbers => true}}
     class SinValidatorCanada
       def initialize(str_value, options)
         @sin = str_value.gsub(/\D/, '') # keep only integers
         @allow_permanent_residents = false
+        @allow_business_numbers = false
 
         country_options = options.fetch(:country_options, {})
         allow_permanent_residents = country_options.fetch(:allow_permanent_residents, :nil)
+        allow_business_numbers = country_options.fetch(:allow_business_numbers, :nil)
 
-        if allow_permanent_residents == true
-          @allow_permanent_residents = true
-        end
+        @allow_permanent_residents = true if allow_permanent_residents == true
+        @allow_business_numbers = true if allow_business_numbers == true
       end
 
       def valid?
-        size_is?(9) && is_not_full_of_zeroes && allow_permanent_residents? && LuhnChecker.valid?(@sin)
+        size_is?(9) && is_not_full_of_zeroes && allow_permanent_residents? && allow_business_numbers? && LuhnChecker.valid?(@sin)
       end
 
       def size_is?(count)
@@ -53,9 +54,19 @@ module ActiveModel
       end
 
       def allow_permanent_residents?
-        permanent_residents_indentifier = "9"
+        permanent_residents_indentifier = '9'
 
         if @allow_permanent_residents == false && @sin.start_with?(permanent_residents_indentifier)
+          false
+        else
+          true
+        end
+      end
+
+      def allow_business_numbers?
+        business_indentifier = '8'
+
+        if @allow_business_numbers == false && @sin.start_with?(business_indentifier)
           false
         else
           true
